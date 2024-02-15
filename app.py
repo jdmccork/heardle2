26d5f351a79f70a993ba2616b97967db116dd9e6
Jack McCorkindale <jdmccork@gmail.com>
2024-02-16 12:12:00 +1300

    Followed tutorial with testing

--- app.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask
-
-def create_app():
-    app = Flask(__name__)
-
-    from apps.hello import hello
-    app.register_blueprint(hello)
-
-    return app
--- apps/hello.py
+++
@@ -1,7 +0,0 @@
-from flask import Blueprint, render_template
-
-hello = Blueprint('hello', __name__)
-
-@hello.route('/hello')
-def index():
-    return render_template('hello.html')
